abb8649597639af59838d29a3c7a8d9842d7bb64
unknown <ascherger@gmail.com>
2012-07-24 17:10:12 -0300

    mi primer comit

---
+++ src/ChatClient.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ChatClient{
+	private JTextArea output;
+	private JTextField input;
+	private JButton sendButton;
+	private JButton quitButton;
+	
+	public ChatClient(){
+		output = new JTextArea(10,50);
+		input = new JTextField(50);
+		sendButton = new JButton("Send");
+		quitButton = new JButton("Quit");
+		
+		//agrego receptor de eventos
+		sendButton.addActionListener(new SendHandler());
+		quitButton.addActionListener(new QuitHandler());
+	}
+	
+	public void launchFrame(){
+		JFrame frame = new JFrame("Chat Room");
+		
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(output,BorderLayout.WEST);
+		frame.add(input,BorderLayout.SOUTH);
+		
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridLayout(2,1));
+		p1.add(sendButton);
+		p1.add(quitButton);
+		
+		frame.add(p1, BorderLayout.CENTER);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	/*public static void main(String[] args){
+		
+		ChatClient c = new ChatClient();
+		c.launchFrame();
+	}*/
+	
+	private class SendHandler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent e){
+			String text = input.getText();
+			output.setText(output.getText()+ text + "\n");
+			input.setText("");
+		}
+	}
+	
+	private class QuitHandler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent e){
+			System.exit(0);
+		}
+	}
+}
---
+++ src/Ingreso.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JSplitPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class Ingreso extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ingreso frame = new Ingreso();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Ingreso() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(108, 11, 68, 14);
+		panel.add(lblUsuario);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(108, 53, 68, 14);
+		panel.add(lblPassword);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(190, 8, 86, 20);
+		panel.add(txtUsuario);
+		txtUsuario.setText("");
+		txtUsuario.setColumns(10);
+		
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.setBounds(87, 95, 89, 23);
+		panel.add(btnIngresar);
+		
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.setBounds(192, 95, 101, 23);
+		panel.add(btnRegistrarse);
+		
+		textField = new JTextField();
+		textField.setBounds(190, 50, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ChatClient c = new ChatClient();
+				c.launchFrame();
+			}
+		});
+	}
+}
